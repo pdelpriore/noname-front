50ab08a5161a9abf00eb91834f1cdc78ffcbae30
pdelpriore <paul.del.priore29@gmail.com>
2022-08-06 23:07:52 +0200

    signout added

--- src/views/profile/Profile.tsx
+++ src/views/profile/Profile.tsx
@@ -1,7 +1,16 @@
 import React from "react";
-import { gql, useQuery } from "@apollo/client";
-import { Avatar, Box, Card, CardHeader, CircularProgress } from "@mui/material";
+import { gql, useQuery, useLazyQuery } from "@apollo/client";
+import {
+  Avatar,
+  Box,
+  Button,
+  Card,
+  CardHeader,
+  CircularProgress,
+} from "@mui/material";
 import { red } from "@mui/material/colors";
+import isLogged from "../../shared/isLogged";
+import { useNavigate } from "react-router-dom";
 
 const GET_ME = gql`
   query Me {
@@ -12,36 +21,65 @@ const GET_ME = gql`
   }
 `;
 
+const USER_SIGNOUT = gql`
+  query Query {
+    userSignOut
+  }
+`;
+
 const Profile: React.FC = () => {
-  const { loading, data } = useQuery(GET_ME);
+  const navigate = useNavigate();
+
+  const { ["loading"]: meLoading, data } = useQuery(GET_ME);
+  const [userSignout, { ["loading"]: userSignoutLoading }] = useLazyQuery(
+    USER_SIGNOUT,
+    {
+      onCompleted: (data) => {
+        if (data?.userSignOut) {
+          localStorage.clear();
+          isLogged(JSON.parse(localStorage.getItem("isLogged") as string));
+          navigate("/", { replace: true });
+        }
+      },
+    }
+  );
+
+  const handleUserSignout = () => userSignout();
 
   const name = data?.me.name;
   const email = data?.me.email;
 
   return (
-    <Box display="flex" justifyContent="center" padding={1}>
-      {!loading ? (
-        <Card
-          sx={{
-            maxWidth: 345,
-            backgroundColor: "#E9ECEF",
-            borderRadius: 2,
-          }}
-          elevation={0}
-        >
-          <CardHeader
-            avatar={
-              <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                {name.charAt(0).toUpperCase()}
-              </Avatar>
-            }
-            title={name}
-            subheader={email}
-          />
-        </Card>
-      ) : (
-        <CircularProgress />
-      )}
+    <Box padding={1}>
+      <Box display="flex" justifyContent="flex-end">
+        {!meLoading ? (
+          <Card
+            sx={{
+              maxWidth: 345,
+              backgroundColor: "#E9ECEF",
+              borderRadius: 2,
+            }}
+            elevation={0}
+          >
+            <CardHeader
+              avatar={
+                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                  {name.charAt(0).toUpperCase()}
+                </Avatar>
+              }
+              title={name}
+              subheader={email}
+            />
+          </Card>
+        ) : (
+          <CircularProgress />
+        )}
+        {!userSignoutLoading ? (
+          <Button onClick={handleUserSignout}>Wyloguj</Button>
+        ) : (
+          <CircularProgress />
+        )}
+      </Box>
     </Box>
   );
 };
